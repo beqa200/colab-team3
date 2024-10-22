54ae7e664f3b689ce94173dcb4ed1136998940e9
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2024-10-22 20:38:15 +0400

    renewed design

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,68 @@
-import React from "react";
-import Login from "./pages/Login";
-import Registration from "./pages/registration";
-import Events from "./pages/events/usersEvents";
-import CreateEvent from "./pages/events/CreateEvent";
-import UpdateEvent from "./pages/events/updateEvent";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Header from "./components/Header";
-
-const queryClient = new QueryClient();
-
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+// import { QueryClientProvider } from 'react-query';
+import Header from './components/Header'
+import Login from './pages/Login'
+import Registration from './pages/Registration';
+// import Events from './pages/events/'
+// import CreateEvent from './CreateEvent';
+// import UpdateEvent from './UpdateEvent';
+import Services from './components/Services';
+import Home from './components/Home';
+import Contact from './components/Contact';
+// Layout component that includes the Header
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet /> {/* This will render the routed component below the Header */}
+    </>
+  );
+}
 
 export default function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Registration />,
+      element: <Layout />,  // Layout wraps the routed components
+      children: [
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path: '/registration',
+          element: <Registration/>
+        },
+        {
+          path:"/services",
+          element:<Services/>
+        },
+        {
+          path: "/contact",
+          element: <Contact/>
+        }
+        // {
+        //   path: '/events',
+        //   element: <Events />,
+        // },
+        // {
+        //   path: '/createEvent',
+        //   element: <CreateEvent />,
+        // },
+        // {
+        //   path: '/updateEvent/:id',
+        //   element: <UpdateEvent />,
+        // },
+      ],
     },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/events",
-      element: <Events />,
-    },
-    {
-      path: "/createEvent",
-      element: <CreateEvent/>
-    },
-    {
-      path: "/updateEvent/:id",
-      element: <UpdateEvent/>
-    }
   ]);
+
   return (
-    
-      <>
-      <Header/>
-       {/* <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}></RouterProvider>
-      
-    </QueryClientProvider> */}
-      </>
-    
    
+      <RouterProvider router={router} />
+    
   );
 }
