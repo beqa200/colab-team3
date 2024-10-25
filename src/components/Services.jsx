e73f71fda77127723f2655c6f239fd1014fdfec5
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2024-10-25 20:06:17 +0400

    services and contact

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,9 +5,16 @@ export default function Services() {
   return (
     <div className='text-center'>
 
-      <div>
-        <h1>
+      <div  >
+        <h1 className='font-poppins mt-[30px] text-gray-500 leading-[35px] text-center whitespace-pre-wrap transition ease duration-[1200ms] delay-[200ms] preFade fadeIn p-[30px] lg:px-[100px]   '>
         We provide expert planning and coordination for a 
+        wide range of events, including birthday parties,
+         weddings, anniversary celebrations, conferences, 
+         concerts,  graduation parties and others. Our team is dedicated
+          to ensuring that each event is tailored to your unique
+           vision, delivering seamless experiences that leave 
+           lasting memories.
+           We provide expert planning and coordination for a 
         wide range of events, including birthday parties,
          weddings, anniversary celebrations, conferences, 
          concerts,  graduation parties and others. Our team is dedicated
@@ -17,77 +24,106 @@ export default function Services() {
         </h1>
       </div>
       <div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
-        <img className=" mt-[50px] w-1/2 md:w-1/3" src={image3} alt="" />
-        <div className='flex-1' >
-          <h1 className=' font-bold mt-[20px]'>Birthday party</h1>
-          <p className='p-[20px]'>
-            It's not just a City Hall wedding. 
-            That's the first thing we want you to
-             know about this venue.  Perfect for you if: 
-              You've dreamed of someplace breathtakingly 
-              beautiful, and stunningly affordable. 
-               Walking down a marble and golden-railed 
-               grand staircase like royalty, makes you giddy.  
-               Words like Grand, Vintage, and Iconic describe 
-               your dream wedding location. 
-          </p>
-          <h2>start planning</h2>
-        </div>
-      </div>
-      <div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8  '>
-        <img className="w-1/2 md:w-1/3 " src={image3} alt="" />
-        <div className='flex-1' >
-          <h1 className='font-bold mt-[20px]' >Birthday party</h1>
-          <p className='p-[20px]' >
-            It's not just a City Hall wedding. 
-            That's the first thing we want you to
-             know about this venue.  Perfect for you if: 
-              You've dreamed of someplace breathtakingly 
-              beautiful, and stunningly affordable. 
-               Walking down a marble and golden-railed 
-               grand staircase like royalty, makes you giddy.  
-               Words like Grand, Vintage, and Iconic describe 
-               your dream wedding location. 
-          </p>
-          <h2 className='font-bold'>start planning</h2>
-        </div>
-      </div>
-      <div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
-        <img className="w-1/2 md:w-1/3" src={image3} alt="" />
-        <div className='flex-1' >
-          <h1 className='font-bold mt-[20px]'>Birthday party</h1>
-          <p className='p-[20px]'>
-            It's not just a City Hall wedding. 
-            That's the first thing we want you to
-             know about this venue.  Perfect for you if: 
-              You've dreamed of someplace breathtakingly 
-              beautiful, and stunningly affordable. 
-               Walking down a marble and golden-railed 
-               grand staircase like royalty, makes you giddy.  
-               Words like Grand, Vintage, and Iconic describe 
-               your dream wedding location. 
-          </p>
-          <h2 className='font-bold' >start planning</h2>
-        </div>
-      </div>
-      <div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
-        <img className="w-1/2 md:w-1/3" src={image3} alt="" />
-        <div className='flex-1' >
-          <h1 className='font-bold mt-[20px]'>Birthday party</h1>
-          <p className='p-[20px]'>
-            It's not just a City Hall wedding. 
-            That's the first thing we want you to
-             know about this venue.  Perfect for you if: 
-              You've dreamed of someplace breathtakingly 
-              beautiful, and stunningly affordable. 
-               Walking down a marble and golden-railed 
-               grand staircase like royalty, makes you giddy.  
-               Words like Grand, Vintage, and Iconic describe 
-               your dream wedding location. 
-          </p>
-          <h2 className='font-bold mt-[20px]'>start planning</h2>
-        </div>
-      </div>
+  <img className="mt-[50px] w-[70%]  md:w-1/3  md:ml-[100px] lg:ml-[200px]" src={image3} alt="" />
+  <div className='flex-1'>
+    <div className="flex justify-center items-center">
+      <h1 className='font-bold mt-[20px] text-[#a2724e] font-poppins text-[30px] text-center  lg:mr-[200px]'>
+        Birthday party
+      </h1>
+    </div>
+    <p className='p-[40px] font-serif text-[#a2724e] lg:mr-[200px] text-[20px]'>
+      It's not just a City Hall wedding. 
+      That's the first thing we want you to
+      know about this venue. Perfect for you if: 
+      You've dreamed of someplace breathtakingly 
+      beautiful, and stunningly affordable. 
+      Walking down a marble and golden-railed 
+      grand staircase like royalty, makes you giddy.  
+      Words like Grand, Vintage, and Iconic describe 
+      your dream wedding location.
+    </p>
+
+    <h2 className="font-poppins ml-[90px] mt-[30px] p-[20px] text-xl rounded-full bg-orange-700 text-white w-[200px] lg:ml-[200px] lg:mb-[20px]">
+      start planning
+    </h2>
+  </div>
+</div>
+
+<div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
+  <img className="mt-[50px] w-1/2 md:w-1/3 md:ml-[100px] lg:ml-[200px]" src={image3} alt="" />
+  <div className='flex-1'>
+    <div className="flex justify-center items-center">
+      <h1 className='font-bold mt-[20px] text-[#a2724e] font-poppins text-[30px] text-center  lg:mr-[200px] lg:mb-[20px]'>
+        Birthday party
+      </h1>
+    </div>
+    <p className='p-[40px] font-serif text-[#a2724e] lg:mr-[200px] text-[20px]'>
+      It's not just a City Hall wedding. 
+      That's the first thing we want you to
+      know about this venue. Perfect for you if: 
+      You've dreamed of someplace breathtakingly 
+      beautiful, and stunningly affordable. 
+      Walking down a marble and golden-railed 
+      grand staircase like royalty, makes you giddy.  
+      Words like Grand, Vintage, and Iconic describe 
+      your dream wedding location.
+    </p>
+
+    <h2 className="font-poppins ml-[90px] mt-[30px] p-[20px] text-xl rounded-full bg-orange-700 text-white w-[200px] lg:ml-[200px]">
+      start planning
+    </h2>
+  </div>
+</div>
+<div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
+  <img className="mt-[50px] w-1/2 md:w-1/3 md:ml-[100px] lg:ml-[200px]" src={image3} alt="" />
+  <div className='flex-1'>
+    <div className="flex justify-center items-center">
+      <h1 className='font-bold mt-[20px] text-[#a2724e] font-poppins text-[30px] text-center  lg:mr-[200px] lg:mb-[20px] cursor-pointer'>
+        Birthday party
+      </h1>
+    </div>
+    <p className='p-[40px] font-serif text-[#a2724e] lg:mr-[200px] text-[20px]'>
+      It's not just a City Hall wedding. 
+      That's the first thing we want you to
+      know about this venue. Perfect for you if: 
+      You've dreamed of someplace breathtakingly 
+      beautiful, and stunningly affordable. 
+      Walking down a marble and golden-railed 
+      grand staircase like royalty, makes you giddy.  
+      Words like Grand, Vintage, and Iconic describe 
+      your dream wedding location.
+    </p>
+
+    <h2 className="font-poppins ml-[90px] mt-[30px] p-[20px] text-xl rounded-full bg-orange-700 text-white w-[200px] lg:ml-[200px] lg:mb-[20px] cursor-pointer">
+      start planning
+    </h2>
+  </div>
+</div>
+<div className='flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8'>
+  <img className="mt-[50px] w-1/2 md:w-1/3 md:ml-[100px] lg:ml-[200px]" src={image3} alt="" />
+  <div className='flex-1'>
+    <div className="flex justify-center items-center">
+      <h1 className='font-bold mt-[20px] text-[#a2724e] font-poppins text-[30px] text-center  lg:mr-[200px] lg:mb-[20px]'>
+        Birthday party
+      </h1>
+    </div>
+    <p className='p-[40px] font-serif text-[#a2724e] lg:mr-[200px] text-[20px]'>
+      It's not just a City Hall wedding. 
+      That's the first thing we want you to
+      know about this venue. Perfect for you if: 
+      You've dreamed of someplace breathtakingly 
+      beautiful, and stunningly affordable. 
+      Walking down a marble and golden-railed 
+      grand staircase like royalty, makes you giddy.  
+      Words like Grand, Vintage, and Iconic describe 
+      your dream wedding location.
+    </p>
+    
+    <h2 className="font-poppins ml-[90px] mt-[30px] p-[20px] text-xl rounded-full bg-orange-700 text-white w-[200px] lg:mb-[20px] lg:ml-[200px] cursor-pointer">
+      start planning
+    </h2>
+  </div>
+</div>
     </div>
   )
 }
